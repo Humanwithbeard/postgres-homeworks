ff4dc11e31d9e7cd0c4aa909b23cf4dcd05cc5a4
denis ukrajev <humanwithbeard@gmail.com>
2023-11-20 16:23:40 +0200

    second done

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -3,8 +3,8 @@
 
 ALTER TABLE products ADD CONSTRAINT chk_products_unit_price CHECK (unit_price > 0)
 SELECT * FROM products
--- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
 
+-- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
 ALTER TABLE products ADD CONSTRAINT chk_products_discontinued CHECK (discontinued IN (0, 1))
 SELECT * FROM products
 
